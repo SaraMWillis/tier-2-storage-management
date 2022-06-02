4340592256a61f7f44f51b79904c27a947e50049
pegasust <pegasucksgg@gmail.com>
2022-06-02 15:47:31 -0700

    fix: example: aws_to_local.sh now copies only 1 file

--- scripts/rclone_ops/aws_to_local.sh
+++ scripts/rclone_ops/aws_to_local.sh
@@ -2,6 +2,6 @@
 
 # AWS back to local
 source .env
-rclone copy AWS:$AWS_BUCKET $SRC_DIR/dest/hello-world.txt
+rclone copy AWS:$AWS_BUCKET/hello-world.txt $SRC_DIR/dest/hello-world.txt
 
 cat $SRC_DIR/dest/hello-world.txt
